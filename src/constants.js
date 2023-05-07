2e99635985199cc85821f4fc3d39c608d6c632bd
Смолянинов Дмитрий Андреевич <smolyaninovda@gmail.com>
2023-05-07 18:56:16 +0800

    14. Generate timeline hours

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,9 @@ export const PAGE_ACTIVITIES = 'activities'
 export const PAGE_PROGRESS = 'progress'
 
 export const NAV_ITEMS = {
-    [PAGE_TIMELINE]: ClockIcon,
-    [PAGE_ACTIVITIES]: ListBulletIcon,
-    [PAGE_PROGRESS]: ChartBarIcon
-  }
+  [PAGE_TIMELINE]: ClockIcon,
+  [PAGE_ACTIVITIES]: ListBulletIcon,
+  [PAGE_PROGRESS]: ChartBarIcon
+}
+
+export const HOURS_IN_DAY = 24
--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,16 +1,21 @@
+<script setup>
+import { generateTimeLineItems } from '../functions'
+
+const timelineItems = generateTimeLineItems()
+</script>
 <template>
   <div class="mt-7">
     <ul>
       <li
-        v-for="i in [0, 1, 2, 3, 4, 5]"
-        :key="i"
+        v-for="{ hour } in timelineItems"
+        :key="hour"
         class="relative flex flex-col gap-2 border-t border-gray-200 px-4 py-10"
       >
         <a
           href="#"
           class="absolute -top-4 left-1/2 -translate-x-1/2 rounded bg-gray-100 px-2 font-mono text-lg text-gray-500"
         >
-          {{ i }}:00
+          {{ hour }}:00
         </a>
       </li>
     </ul>
