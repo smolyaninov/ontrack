a92f9f52a8e0d5c375b05f8dfb98984c1b0ad0e1
Смолянинов Дмитрий Андреевич <smolyaninovda@gmail.com>
2023-05-08 10:47:18 +0800

    15. TimelineItem component

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,23 +1,17 @@
 <script setup>
 import { generateTimeLineItems } from '../functions'
+import TimelineItem from '../components/TimelineItem.vue'
 
 const timelineItems = generateTimeLineItems()
 </script>
 <template>
   <div class="mt-7">
     <ul>
-      <li
-        v-for="{ hour } in timelineItems"
-        :key="hour"
-        class="relative flex flex-col gap-2 border-t border-gray-200 px-4 py-10"
-      >
-        <a
-          href="#"
-          class="absolute -top-4 left-1/2 -translate-x-1/2 rounded bg-gray-100 px-2 font-mono text-lg text-gray-500"
-        >
-          {{ hour }}:00
-        </a>
-      </li>
+      <TimelineItem
+        v-for="timelineItem in timelineItems"
+        :key="timelineItem.hour"
+        :timeline-item="timelineItem"
+      />
     </ul>
   </div>
 </template>
